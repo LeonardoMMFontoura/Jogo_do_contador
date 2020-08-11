0c89bc2734155e915899b745e643128c1fe5c36e
Leonardo Monteiro Mastra Fontoura <leonardommfontoura@gmail.com>
2020-08-11 15:32:41 -0300

    logica de fim de jogo adicionada

--- score_keeper.js
+++ score_keeper.js
@@ -6,14 +6,25 @@ let p2Display = document.querySelector("#p2Display");
 p1_pont = 0;
 p2_pont = 0;
 
+let gameOver = false;
+let venceu = 5;
 
 p1.addEventListener("click", function() {
-    p1_pont++;
-    p1Display.textContent = p1_pont;
+    if(!gameOver){
+        p1_pont++;
+        if(p1_pont == venceu){
+            gameOver = true;
+        }
+        p1Display.textContent = p1_pont;
+    }
 });
 
 
 p2.addEventListener("click", function() {
-    p2_pont++;
+    if(!gameOver)
+        p2_pont++;
+        if(p2_pont == venceu){
+            gameOver = true;
+        }
     p2Display.textContent = p2_pont;
 });
