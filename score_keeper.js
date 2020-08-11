448c73b7fcd801fef10c3ef87ff89bfc42acd75a
Leonardo Monteiro Mastra Fontoura <leonardommfontoura@gmail.com>
2020-08-11 15:26:01 -0300

    logica de pontuacao dos dois jogadores adicionadas

--- score_keeper.js
+++ score_keeper.js
@@ -1,11 +1,19 @@
 let p1 = document.querySelector("#p1");
 let p2 = document.querySelector("#p2");
 let p1Display = document.querySelector("#p1Display");
-p1_pont = 0;
+let p2Display = document.querySelector("#p2Display");
 
+p1_pont = 0;
+p2_pont = 0;
 
 
 p1.addEventListener("click", function() {
     p1_pont++;
     p1Display.textContent = p1_pont;
+});
+
+
+p2.addEventListener("click", function() {
+    p2_pont++;
+    p2Display.textContent = p2_pont;
 });
