8e8a195650f3658427dabbb8d050c98812d025c2
Leonardo Monteiro Mastra Fontoura <leonardommfontoura@gmail.com>
2020-08-11 15:19:53 -0300

    adicionando mudança na pontuacao do jogo

--- score_keeper.js
+++ score_keeper.js
@@ -1,7 +1,11 @@
 let p1 = document.querySelector("#p1");
 let p2 = document.querySelector("#p2");
+let p1Display = document.querySelector("#p1Display");
+p1_pont = 0;
+
 
 
 p1.addEventListener("click", function() {
-    alert("clicked!");
+    p1_pont++;
+    p1Display.textContent = p1_pont;
 });
